cb158d7c2a6b571a32afeb98d68153e6c4872cb2
Josue Baldeon Araujo <136621348+Klell11@users.noreply.github.com>
2025-07-23 22:06:49 -0500

    lstvoluntario

--- ListadoVoluntario.aspx.cs
+++ ListadoVoluntario.aspx.cs
@@ -1,1 +1,108 @@
-﻿
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace WebApplication2
+{
+    public partial class ListadoVoluntario : System.Web.UI.Page
+    {
+        private readonly string cadena =
+            ConfigurationManager.ConnectionStrings["conexionDatos"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            bool esAdmin = Session["usuario"] != null &&
+                           Session["rol"] != null &&
+                           Session["rol"].ToString() == "administrador";
+
+            ViewState["EsAdmin"] = esAdmin;
+            btnAgregar.Visible = esAdmin;
+
+            if (gvVoluntario.Columns.Count > 0)
+            {
+                gvVoluntario.Columns[gvVoluntario.Columns.Count - 1].Visible = esAdmin;
+            }
+
+            if (!IsPostBack)
+            {
+                CargarVoluntarios();
+                if (Session["mensaje"] != null)
+                {
+                    Response.Write($"<script>alert('{Session["mensaje"]}');</script>");
+                    Session.Remove("mensaje");
+                }
+            }
+        }
+
+        private void CargarVoluntarios()
+        {
+            var dt = new DataTable();
+            using (var cn = new MySqlConnection(cadena))
+            using (var cmd = new MySqlCommand("sp_consultar_voluntario", cn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                using (var da = new MySqlDataAdapter(cmd))
+                    da.Fill(dt);
+            }
+
+            gvVoluntario.DataSource = dt;
+            gvVoluntario.DataBind();
+
+            if (gvVoluntario.HeaderRow != null)
+            {
+                gvVoluntario.UseAccessibleHeader = true;
+                gvVoluntario.HeaderRow.TableSection = TableRowSection.TableHeader;
+            }
+        }
+
+        protected void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("AgregarVoluntario.aspx", true);
+        }
+
+        protected void gvVoluntario_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            bool esAdmin = ViewState["EsAdmin"] != null && (bool)ViewState["EsAdmin"];
+            if (!esAdmin) return;
+
+            int id = Convert.ToInt32(e.CommandArgument);
+
+            if (e.CommandName == "Editar")
+            {
+                Response.Redirect($"EditarVoluntario.aspx?id={id}", true);
+            }
+            else if (e.CommandName == "Eliminar")
+            {
+                using (var cn = new MySqlConnection(cadena))
+                using (var cmd = new MySqlCommand("sp_eliminar_voluntario", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("p_id_voluntario", id);
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+
+                CargarVoluntarios();
+            }
+        }
+
+        protected void gvVoluntario_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                bool esAdmin = ViewState["EsAdmin"] != null && (bool)ViewState["EsAdmin"];
+
+                if (!esAdmin)
+                {
+                    var btnEditar = e.Row.FindControl("btnEditar") as Button;
+                    var btnEliminar = e.Row.FindControl("btnEliminar") as Button;
+
+                    if (btnEditar != null) btnEditar.Visible = false;
+                    if (btnEliminar != null) btnEliminar.Visible = false;
+                }
+            }
+        }
+    }
+}
